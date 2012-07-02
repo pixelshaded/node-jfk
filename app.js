f6263f4a2b609c6f1c735046c959528f2095f432
Alexander Fisher <alexanderfisher@Alexander-Fisher-2.local>
2012-07-02 17:18:17 -0600

    Added twig port. Removed jade from package. Added directory for twig port to config. Config is now a field on app object.

--- app.js
+++ app.js
@@ -6,12 +6,12 @@
 express = require('express');
 app = module.exports = express.createServer();
 
-var config = require('./config/config');
+app.config = require('./config/config');
 var cwd = process.cwd();
 
-require(cwd + config.folders.environment + '/global');
-require(cwd + config.folders.environment + '/' + app.settings.env);
-require(cwd + '/route-mapper')(cwd, config.folders.route, config.folders.controller);
+require(cwd + app.config.folders.environment + '/global');
+require(cwd + app.config.folders.environment + '/' + app.settings.env);
+require(cwd + '/route-mapper')(cwd, app.config.folders.route, app.config.folders.controller);
 
 app.listen(3000, function(){
   console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
--- config/environments/global.js
+++ config/environments/global.js
@@ -2,12 +2,12 @@ app.configure(function(){
    console.log('Configuring global environment.');
    
    var cwd = process.cwd();
+   var twig = require(cwd + app.config.folders.twig);
    
    //load global modules
    fs = require('fs');
    
-   app.set('views', cwd + '/views');
-   app.set('view engine', 'jade');
+   app.register('twig', twig);
    
    app.use(express.static(cwd + '/public')) ;
    app.use(express.favicon());
@@ -15,4 +15,8 @@ app.configure(function(){
    app.use(express.bodyParser());
    app.use(express.methodOverride());
    app.use(app.router);
+   
+   app.set('views', cwd + '/views');
+   app.set('view engine', 'twig');
+   app.set("view options", { layout: false });
 });
